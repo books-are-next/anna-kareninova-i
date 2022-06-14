7ea578ca749ab714e52d1043811dd0123a7d84dc
Jan Martinek <honza.martinek@gmail.com>
2022-06-14 08:21:53 +0200

    Self-update

---
+++ self-update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# Remove old files
+
+rm -rf layouts assets static config.toml README.* package.json package-lock.json filters.js webpack.config.js
+
+
+# Get current boilerplate code from repo
+
+shopt -s dotglob
+git clone https://github.com/next-book/boilerplate.git _current_bp
+
+
+# Move new files into place
+
+mv _current_bp/layouts layouts
+mv _current_bp/assets assets
+mv _current_bp/static static
+mv _current_bp/README.* .
+mv _current_bp/config.toml .
+mv _current_bp/package.json .
+mv _current_bp/package-lock.json .
+mv _current_bp/webpack.config.js webpack.config.js
+mv _current_bp/filters.js filters.js
+mv _current_bp/calc-accent.mjs calc-accent.mjs
+mv _current_bp/self-update.sh self-update.sh
+
+
+# Remove imported repo dir
+
+rm -rf _current_bp
